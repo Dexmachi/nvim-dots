cba5b236ec6e92b030aef68431b12b5353dc6ee3
Caio Rocha <232001371@aluno.unb.br>
2025-07-29 17:00:05 -0300

    claridade em autocomplete.lua e plugin de markdown viewer melhor

--- nvim/lua/plugins/autocomplete.lua
+++ nvim/lua/plugins/autocomplete.lua
@@ -3,6 +3,8 @@ return {
     "L3MON4D3/LuaSnip",
     opts = function(_, opts)
       require("luasnip.loaders.from_lua").lazy_load({
+        -- SE TOCA, COLOCA OS SNIPPETS NESSE DIRETÓRIO AQUI EMBAIXO
+        -- E NÃO SE ESQUECE, O NOME DO (snippet).lua TEM QUE SER O MESMO DA LINGUAGEM
         paths = { "~/.config/nvim/lua/snippets" },
       })
       require("luasnip.loaders.from_vscode").lazy_load()
